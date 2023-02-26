388e17b2895bb705c344c3f248ddac2f8adeb448
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-26 16:59:16 -0600

    Operadores Exponentes

--- 26_02_2023_007_Operadores_2_Exponentes.py
+++ 26_02_2023_007_Operadores_2_Exponentes.py
@@ -1,3 +1,4 @@
 #Exponentes para usar los exponentes se usa el operador ** que es lo mismo que "^"
-resultado=2*2*2*2*2*2*2
-print("El resultado es : "resultado)
+resultado=2*2*2*2*2*2*2*2*2*2
+resultado1=2**10
+print("El resultado es de multiplicar 2 por si mismo 10 veces es:",resultado, 'tambien pudes obtener este resultado usando "**":', resultado1)
