d3f9cb662127aed42965d5b17202a2b13c1eee3c
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-25 20:17:42 -0600

    esta es una prueba para ver como se trabaja visual y github juntos

---
+++ 25_02_2023_001_Variables.py
@@ -0,0 +1,5 @@
+mensaje="El resutado de la suma es:"
+num=5
+num2=10
+resultado=num+num2
+print(mensaje,resultado)
---
+++ 25_02_2023_002_Strings1.py
@@ -0,0 +1,4 @@
+mensaje1='"print()"'
+mensaje2=" se utiliza para imprimir valores en la consola."
+mensaje2=mensaje1+mensaje2
+print(mensaje2)
