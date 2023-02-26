a03a7bf74c05567e6b7d645e5238a11c487af291
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-26 13:28:02 -0600

    String

---
+++ 25_02_2023_003_Strings_2_Contatenar.py
@@ -0,0 +1,10 @@
+mensaje="Esta es"+" una prueba"
+print(mensaje)
+mensaje1="para saber como"
+mensaje2=" se concatenan"
+mensaje3=mensaje1+mensaje2
+print(mensaje3+' los strings usando "+"')
+nombre="Cesar Hernando"
+apellido="Torres Soriano"
+edad="2"
+print(nombre+" "+apellido+" tengo "+2*edad+" años")
