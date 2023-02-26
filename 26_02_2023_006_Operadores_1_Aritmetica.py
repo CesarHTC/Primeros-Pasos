ab2ab08acf938b2bd7f9f17b28b35ba534e0e04a
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-26 16:49:23 -0600

    Aritmetica Operadores

---
+++ 26_02_2023_006_Operadores_1_Aritmetica.py
@@ -0,0 +1,13 @@
+import numpy as np
+suma=20+23+44 #resultado 87
+resta=20-23-84 #resultado -87
+#uso el metodo roun para rendondear la cantidad y quitarle los decimales
+multiplicacion=np.round(20*23*1.8913)#resultado 870
+division=np.round(5000/230/2.2) #resultado 10
+operacion=10/5+15-17 #resultado 0
+#muestro el resultado
+print("El resultado de la suma es: ",suma)
+print("El resultado de la resta es: ",resta)
+print("El resultado de la multiplicacion es: ",multiplicacion)
+print("El resultado de la divicion es: ",division)
+print("El resultado de la operacion es: ",operacion)
