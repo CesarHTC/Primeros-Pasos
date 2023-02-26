aebafc8faac37acef3a724e22c441e4990038081
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-26 17:56:37 -0600

    Flotante metodo round

---
+++ 26_02_2023_008_Flotantes.py
@@ -0,0 +1,5 @@
+import numpy as np
+flotante=17.567383292929200234
+#round ees un metodo de la libreria numpy que nos ayuda a redondear numeros 
+#le podemos especificar hata que decimal queremos que redonde 
+print(np.round(flotante,5))
