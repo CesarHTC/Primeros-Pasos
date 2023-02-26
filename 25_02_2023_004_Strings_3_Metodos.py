f1b0e74ca1b65d2fb10ee2f15cf911c4c0e45d2a
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-26 13:43:47 -0600

    Strings Metodos

---
+++ 25_02_2023_004_Strings_3_Metodos.py
@@ -0,0 +1,8 @@
+nombre="enrique barros fernández"
+#El metodo title transforma la primera letra de cada palabra en mayuscula
+print(nombre.title())
+frase="Esta Es Una Frase Para Ser Formateada por los metodos de los sTrINgS."
+#el metodo lower transforma las variables en minisculas
+print(frase.lower())
+#El metodo upper transforma las letras en mayusculas
+print(frase.upper())
