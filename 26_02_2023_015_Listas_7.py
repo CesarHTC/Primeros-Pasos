edf0ae48a8821140b6e0eab72627f974c820556d
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-26 20:31:47 -0600

    Arrays

---
+++ 26_02_2023_015_Listas_7.py
@@ -0,0 +1,7 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+#El metodo insert nos permite insertar un valor en una posicion expecifica del array
+print(colores)
+colores.insert(-4,"magenta")
+colores.insert(-1,"turquesa")
+
+print("\nLa nueva lista es :", colores)
