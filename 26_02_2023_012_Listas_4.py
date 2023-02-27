307801584255f837fb62a858f8cd253995912540
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-26 20:02:52 -0600

    Arrays

---
+++ 26_02_2023_012_Listas_4.py
@@ -0,0 +1,6 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(colores)
+#el metodo remove nos permite borrar un elemento sin conocer su numero de indice
+colores.remove("amarillo")
+colores.remove("rojo")
+print("La nueva lista:",colores)
