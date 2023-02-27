fe256d03df4101d7f53c501bd057cff10cf7624d
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-26 19:31:43 -0600

    Arrays

---
+++ 26_02_2023_009_Listas_1.py
@@ -0,0 +1,6 @@
+#Una lista, es una variable con múltiples valores.
+lista=["tres","dos","cinco","cuatro","uno"]
+#puedes imprimir toda la lista 
+print(lista)
+#O valores especificos de ella
+print(lista[2])
