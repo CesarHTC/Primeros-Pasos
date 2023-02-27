9854a7588c6db6d687b71f7879f7448317b4c37e
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-26 19:32:01 -0600

    Arrays

--- 26_02_2023_009_Listas_1.py
+++ 26_02_2023_009_Listas_1.py
@@ -3,4 +3,6 @@ lista=["tres","dos","cinco","cuatro","uno"]
 #puedes imprimir toda la lista 
 print(lista)
 #O valores especificos de ella
+#aqui imprimimos el valor de la posicion 2 de la lista (cinco)
+#Nota se empieza a contar desde 0
 print(lista[2])
