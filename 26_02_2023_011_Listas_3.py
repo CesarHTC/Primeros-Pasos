b0e9b12269bacc958ce77beab5d9329fbecbb089
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-26 19:56:10 -0600

    Arrays

---
+++ 26_02_2023_011_Listas_3.py
@@ -0,0 +1,8 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(colores)
+#El metodo "del[]" nos permite eliminar elementos de una lista 
+del colores[1]
+del colores[4]
+del colores[-4]
+del colores[-3]
+print("El array colores despues de borrar algunos elementas:",colores)
