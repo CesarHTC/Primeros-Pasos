b54cd64f1c8fa3d78e569828d0757c6ad2be04f0
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-26 20:22:17 -0600

    Arrays

--- 26_02_2023_014_Lista_6.py
+++ 26_02_2023_014_Lista_6.py
@@ -1,5 +1,6 @@
 colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
 #Append es un metodo que nos permite insertar valores a un lista
+#los inserta el la ultima posicion
 colores.append("fuxia")
 colores.append("celeste")
 print("La nueva lista:",colores)
