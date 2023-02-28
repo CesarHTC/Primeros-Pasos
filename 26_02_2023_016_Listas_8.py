77f0a5bc0a2996a7a6c0b40e3b3305c205070d33
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-27 22:38:46 -0600

    arrays

---
+++ 26_02_2023_016_Listas_8.py
@@ -0,0 +1,4 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón','lila', 'negro', 'rosa', 'blanco', 'naranja']
+#Sort nos permite ordenar alfabeticamente una lista
+colores.sort(reverse=True)
+print("La lista ordenada alfabeticamente:",colores)
