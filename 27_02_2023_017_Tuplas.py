4d8345b5cc8b762302205d07f6855906ec8f789b
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-27 22:58:52 -0600

    Tuplas

---
+++ 27_02_2023_017_Tuplas.py
@@ -0,0 +1,7 @@
+#las tuplas son parecidas a las lista pero con la diferecia
+#de que estas no se pueden modificar
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+#se pueden acceder a los datos pero no se puden modificar
+print(colores[1])
+numeros = (10, 1, 5, 11)
+print("El resusltado es: ",(numeros[0]+numeros[0]+numeros[2])) # 25 
---
+++ 27_02_2023_018_Tuplas_2_Convertir.py
@@ -0,0 +1,5 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+#las tuplas se pueden transformar en listas y viceversa
+tupla=tuple(colores)
+#la funcion type te dice el formato del tipo de dato con el que trabajas
+print("Colores es un array de tipo: ",type(colores)," Tupla es un array de tipo:",type(tupla))
