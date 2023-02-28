4192b7d82fd1cbb7f9f7e70ef444988be58cb2c9
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-02-27 23:21:18 -0600

    Condicionales If

---
+++ 27_02_2023_019_Condicionales_1_If.py
@@ -0,0 +1,47 @@
+#El bloque de código if va a ejecutar un determinado código solo si se cumplen
+#una o varias condiciones. Esto nos da la enorme ventaja de que nuestro 
+#programa pueda tener resultados diferentes.
+
+#Ejercicio 36
+"""
+num1 = 15
+num2 = 20
+
+if num1 == num2:
+	print('Se ejecuta el if.')
+"""
+#Cambio de condicional
+num1 = 15
+num2 = 20
+
+if num1 < num2:
+	print('Se ejecuta el if ya que 15 es menor que 20.')
+#Ejercicio 37
+"""
+num1 = 1450
+num2 = 60
+
+if num1 < num2:
+	print('Se ejecuta el if.')
+"""
+#Cambio de condicional
+num1 = 1450
+num2 = 60
+
+if num1 > num2:
+	print('Se ejecuta el if ya que el numero 1 es mayor que el numero 2.')
+#Ejercicio 38
+"""
+num1 = 1450
+num2 = 60
+
+if num1 != num2:
+	print('Se ejecuta el if.')
+"""
+#Cambio de condicional
+num1 = 60
+num2 = 60
+
+if num1 != num2:
+	print('Se ejecuta el if ya que el numero 1 es mayor que el numero 2.')
+ 
