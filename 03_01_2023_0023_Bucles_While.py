a866f67755ae1eb3e1c245d4823e88b05d488787
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-03-01 12:15:07 -0600

    Bucles while

---
+++ 03_01_2023_0023_Bucles_While.py
@@ -0,0 +1,19 @@
+#Crea un bucle while que se ejecute hasta que x valga 15 con incrementos de 5
+x = 0
+while x <= 15:
+	print("El valor de x es de:",x)
+	x += 5# la variable x incrementa de 5 en 5
+#Crea un bucle while que se ejecute hasta que x valga -100 con decrementos de 20.
+x = 0
+print("\n")
+while x >= -100:
+	print("El valor de x es de:",x)
+	x -=20
+#Crea un bucle while que se ejecute hasta que x valga 0 con decrementos de 1 y 
+#que muestre en cada ejecución esta frase con el valor de ejecución correspondiente:
+# 'El valor del bucle es 10'..
+print("\n")
+x=10
+while x >= 0:
+	print("El valor del bucle es de:",x)
+	x -=1
