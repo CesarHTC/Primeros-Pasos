2afddd82ca9aaee2ffd2b4282a9611a37ad6ab02
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-03-01 11:20:23 -0600

    Condicional if-else

---
+++ 01_03_2023_020_Condicionales_2_If_Else.py
@@ -0,0 +1,19 @@
+#corrige el siguiente codigo
+"""
+color = rojo
+
+else color == rojo
+Print "El color es rojo."
+if color != rojo
+Print "El color no es rojo.
+"""
+#color debe ser un string
+color = "rojo"
+#primero debe ir el if antes que else
+if color == 'rojo':
+#la funcion print() esta mal
+#el codigo esta mal indexado y else no lleva condicion y lleba :
+    print("El color es rojo.")
+else:
+    print("El color no es rojo.")
+#si cambiamos el string que guarda la variable color se ejecutara la secion de codigo del else
