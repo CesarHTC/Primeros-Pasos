97668208dacf1e577dbffa4194290e4f67bb2776
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-03-06 11:32:23 -0600

    Funciones

---
+++ 03_06_2023_030_Funciones.py
@@ -0,0 +1,12 @@
+#las funciones de Python, las cuales son bloques de código que solo se van a ejecutar si se les llama.
+#Los argumentos en las funciones son variables que podemos especificar por defecto al llamar a una función.
+#Crea una función que realice una suma.
+def suma(num, num2):
+    print('Hola el resultado de la suma es', num+num2)
+
+
+#Aqui mando a llamar a la funcion suma y le paso los dos datos
+# con los que va a trabajar 
+suma(29,1)
+suma(30,20)
+suma((57000/2),(57000/2))
