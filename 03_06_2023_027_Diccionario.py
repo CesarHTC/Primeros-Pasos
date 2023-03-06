9f9d08776ed8b715f21c32979f1f8d3cbc3cf034
CesarHTC <126126598+CesarHTC@users.noreply.github.com>
2023-03-06 11:10:42 -0600

    Diccionarios

---
+++ 03_06_2023_027_Diccionario.py
@@ -0,0 +1,17 @@
+#Python tiene un tipo de dato llamado diccionario, el cuál es capaz de almacenar en él una colección de objetos.
+
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+#Del diccionario teclado2 del capítulo,
+#muestra los elementos Modelo y Precio con presentación en un print(). 
+print("El modelo ",teclado2['Modelo'],"cuesta:",teclado2['Precio'],"$")
